30bdd2ecc0b39601c1356196507b936bd58f6153
Abdulazeez Ibrahim <48849318+skygifted@users.noreply.github.com>
2023-02-15 01:27:37 +0200

    Add files via upload
    m

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div  className="App">
+      
+      <header><h1>LOGO</h1></header>
+       <nav>
+         <ul>
+            <li>Contact</li>
+            <li>About</li>
+            <li>Programs</li>
+         </ul>
+       </nav>
+
+      <section>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam voluptatum veritatis earum provident. Excepturi exercitationem est necessitatibus voluptatibus possimus cum dicta ad a, sequi aperiam atque quo, eligendi consequuntur. Eaque alias porro natus qui cupiditate voluptatibus expedita assumenda similique, nobis quae nihil asperiores distinctio necessitatibus eligendi inventore sit, error architecto reprehenderit veritatis aperiam nulla corporis repudiandae iusto? Est iure non incidunt, vitae sequi error sapiente officia iusto ipsam beatae obcaecati?
+      </section>
+
+      <footer>
+       <li>Terms and Service</li>
+
+      </footer>
+
+    </div>
+      
+  );
+}
+
+export default App;
